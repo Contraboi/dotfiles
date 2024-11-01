8494fd93d01757e6f0a511361bf76934a5f09e7b
Selmir Nedzibi <Contraboi@users.noreply.github.com>
2024-11-01 13:27:07 +0100

    feat: change colors from mocha to tokyonight & add tokyonight bg

--- nvim/.config/nvim/lua/contraboi/lazy/utils.lua
+++ nvim/.config/nvim/lua/contraboi/lazy/utils.lua
@@ -13,7 +13,9 @@ local utils = {
   { 'github/copilot.vim' },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim',      opts = {} },
+
+  { 'norcalli/nvim-colorizer.lua' },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
