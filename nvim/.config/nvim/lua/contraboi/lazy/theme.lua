648180016ddad8867173c6f42cea1e93e2fa689f
Selmir Nedzibi <Contraboi@users.noreply.github.com>
2024-11-01 13:21:55 +0000

    nvim tokyionight

--- nvim/.config/nvim/lua/contraboi/lazy/theme.lua
+++ nvim/.config/nvim/lua/contraboi/lazy/theme.lua
@@ -10,22 +10,6 @@ end
 
 return {
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      require('rose-pine').setup {
-        disable_background = true,
-        styles = {
-          italic = false,
-        },
-      }
-
-      vim.cmd 'colorscheme rose-pine'
-
-      ColorMyPencils 'rose-pine'
-    end,
-  },
-  {
     'folke/tokyonight.nvim',
     config = function()
       require('tokyonight').setup {
