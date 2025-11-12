73c9f062f62a805aacc82a876c8c7436a10be95e
Selmir Nedzibi <selmir.nedzibi99@gmail.com>
2025-11-12 18:59:17 +0100

    feat: supermaven

--- nvim/.config/nvim/lua/contraboi/lazy/utils.lua
+++ nvim/.config/nvim/lua/contraboi/lazy/utils.lua
@@ -11,11 +11,16 @@ local utils = {
   --  This is equivalent to:
   --    require('Comment').setup({})
 
-  { 'github/copilot.vim' },
-  { 'dmmulroy/tsc.nvim',          opts = {} },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
+  { 'dmmulroy/tsc.nvim', opts = {} },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',      opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   { 'norcalli/nvim-colorizer.lua' },
 
@@ -41,23 +46,33 @@ local utils = {
         map('n', '<leader>hp', gs.preview_hunk, { desc = '[H]unk [P]review' })
         map('n', '<leader>hr', gs.reset_hunk, { desc = '[H]unk [R]eset' })
         map('n', '<leader>hs', gs.stage_hunk, { desc = '[H]unk [S]stage' })
-        map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-          { desc = '[H]unk [R]eset' })
-        map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = '[H]unk [B]lame' })
-
+        map('v', '<leader>hr', function()
+          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[H]unk [R]eset' })
+        map('n', '<leader>hb', function()
+          gs.blame_line { full = true }
+        end, { desc = '[H]unk [B]lame' })
 
         -- Navigate hunks
         map('n', ']h', function()
-          if vim.wo.diff then return ']h' end
-          vim.schedule(function() gs.next_hunk() end)
+          if vim.wo.diff then
+            return ']h'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
           return '<Ignore>'
         end, { expr = true, desc = 'Next Hunk' })
 
         map('n', '[h', function()
-          if vim.wo.diff then return '[h' end
-          vim.schedule(function() gs.prev_hunk() end)
+          if vim.wo.diff then
+            return '[h'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
           return '<Ignore>'
-        end, { expr = true, desc = "Prev Hunk" })
+        end, { expr = true, desc = 'Prev Hunk' })
       end,
       signs = {
         add = { text = '+' },
